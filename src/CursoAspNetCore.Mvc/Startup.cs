e6cbb6ff18ed30c8b34e550bf48db4ac1048b371
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-07-04 16:04:50 -0300

    commit

--- src/CursoAspNetCore.Mvc/Startup.cs
+++ src/CursoAspNetCore.Mvc/Startup.cs
@@ -34,26 +34,23 @@ namespace CursoAspNetCore.Mvc {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
             services.AddMvc ();
+            
+            services.AddTransient<IClienteRepository, ClienteRepository>();
+            services.AddTransient<IEnderecoRepository, EnderecoRepository>();
 
-            services.AddAutoMapper ();
+            services.AddTransient<IEnderecoAppService, EnderecoAppService>();
+            services.AddTransient<IClienteAppService, ClienteAppService>();
 
-            services.AddSingleton<IControllerActivator> (
-                new SimpleInjectorControllerActivator (container));
-            services.AddSingleton<IViewComponentActivator> (
-                new SimpleInjectorViewComponentActivator (container));
+            services.AddTransient<IClienteService, ClienteService>();      
+            services.AddTransient<IClienteService, ClienteService>(); 
 
-            services.UseSimpleInjectorAspNetRequestScoping (container);
+            services.AddTransient<CursoAspNetCoreContext>();  
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
-            InitializeContainer (app);
-
-            //container.Register<CustomMiddleware1>();
-            //container.Register<CustomMiddleware2>();
-
-            container.Verify ();
+           
 
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
@@ -69,38 +66,6 @@ namespace CursoAspNetCore.Mvc {
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
-        private void InitializeContainer (IApplicationBuilder app) {
-
-            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle ();
-
-            // Add application presentation components:
-            container.RegisterMvcControllers (app);
-            container.RegisterMvcViewComponents (app);
-
-            // Add application services. For instance:
-            // container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Scoped);
-
-            //App
-            container.Register<IClienteAppService, ClienteAppService> (Lifestyle.Scoped);
-            container.Register<IEnderecoAppService, EnderecoAppService> (Lifestyle.Scoped);
-
-            //Domain
-            //container.Register(typeof(IServiceBase<>), typeof(ServiceBase<>));
-            container.Register<IClienteService, ClienteService> (Lifestyle.Scoped);
-            container.Register<IEnderecoService, EnderecoService> (Lifestyle.Scoped);
-
-            //Infra Dados
-            container.Register (typeof (IRepository<>), typeof (RepositoryBase<>));
-            container.Register<IClienteRepository, ClienteRepository> (Lifestyle.Scoped);
-            container.Register<IEnderecoRepository, EnderecoRepository> (Lifestyle.Scoped);
-            container.Register<IUnitOfWork, UnitOfWork> (Lifestyle.Scoped);
-
-            //container.Register<DbContextOptionsBuilder<CursoAspNetCoreContext>> (.LifestyleScoped);
-            container.Register(typeof(DbContextOptionsBuilder<>));
-
-            container.Register<DbContextOptionsBuilder>(() => new DbContextOptionsBuilder<CursoAspNetCoreContext>());
-          
-            
-        }
+       
     }
 }
