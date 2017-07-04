dae6e2427bb04471b94b47367f3cd30973ddbe5f
Alessandro de Deus Silveira <alesilver@hotmail.com>
2017-07-03 21:23:02 -0300

    commit

---
+++ CursoAspNetCore.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CursoAspNetCore.Application.ViewModel;
+using CursoAspNetCore.Domain.Entities;
+
+namespace CursoAspNetCore.Application.AutoMapper
+{
+	internal class DomainToViewModelMappingProfile: Profile
+	{
+		
+			public DomainToViewModelMappingProfile()
+			{
+				CreateMap<Cliente, ClienteViewModel>();
+				CreateMap<Endereco, EnderecoViewModel>();
+			}
+		
+	}
+}
